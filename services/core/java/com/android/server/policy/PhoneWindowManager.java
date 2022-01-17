AospExtended/platform_frameworks_base

Hardware key: Customization support [1/2]
Based on DUI

xyyx:
-adapted to P
-moved utils to utils
-res to SystemUI
-changed animation for lastapp
-removed pinned action
-commented removing from recents
-disabled some actions for now

jhenrique09 edits: make it DUI independent

Original commit:
Squashed commit of the following:

Manually bring in

DUI: Migrate to API/Permission model for Editor [1/5]
bigrushdog/android_frameworks_base@20681de

commit d3dd2b5df493989f3bbb479a5734df3a84d9598a
Author: bigrushdog <randall.rushing@gmail.com>
Date:   Fri Aug 11 18:15:22 2017 -0700

    DUtils: Migrate from broadcasts to API for some action handling [1/2]

    Protected broadcasts for ActionHandler actions will become problematic
    in the future. Currently, broadcasts are sent from SystemUI as well as
    system server (hwkeys). This may also change in the future. So provide
    a general API solution with properly secured public methods.

    Change-Id: I90ee0dd5fe75545d50ca77dd0fecba86ab23a09e

commit 6a528e1261649a404ee70ea74dda1551f53a30e1
Author: bigrushdog <randall.rushing@gmail.com>
Date:   Wed Aug 2 16:10:54 2017 -0700

    DUI: Left in landscape navbar API [1/3]

    Window manager will let SystemUI know when navbar
    enters or exits the "left in landscape" position.
    We tried to avoid this using orientation listeners
    but that approach proved unreliable leaving Pulse
    in bizarre states when navbar was left in landscape

    Change-Id: Ie67b905755cd20219f08e76232a10fde50ec9b85

commit 7e8cb8b0cfa47d0b1e1105f97905167efafac851
Author: Randall Rushing <randall.rushing@gmail.com>
Date:   Thu Oct 29 05:02:04 2015 -0700

    Pulse: forward port visualizer lock api

    Change-Id: I13c7bbc8f4dc79fa41f11cbab25c1cdd83aed2af

commit 593d60fcea0a2edfebb1ed9ebc21ab69238cb7e7
Author: Randall Rushing <randall.rushing@gmail.com>
Date:   Thu Jan 7 02:41:02 2016 -0800

    statusbar: add screenPinningStateChanged(boolean enabled) api and pipe to navigation bar

    Let

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -211,6 +211,10 @@ import com.android.internal.statusbar.IStatusBarService;
 import com.android.internal.util.ArrayUtils;
 import com.android.server.ExtconStateObserver;
 import com.android.server.ExtconUEventObserver;
+import com.android.internal.util.hwkeys.ActionHandler;
+import com.android.internal.util.hwkeys.ActionUtils;
+import com.android.internal.util.ScreenshotHelper;
+import com.android.internal.util.ScreenShapeHelper;
 import com.android.server.GestureLauncherService;
 import com.android.server.LocalServices;
 import com.android.server.SystemServiceManager;
@@ -501,6 +505,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     int mVeryLongPressTimeout;
     boolean mAllowStartActivityForLongPressOnPowerDuringSetup;
     MetricsLogger mLogger;
+    private HardkeyActionHandler mKeyHandler;
 
     private boolean mHandleVolumeKeysInWM;
 
@@ -779,6 +784,16 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     dispatchMediaKeyWithWakeLockToAudioService(
                             KeyEvent.changeAction(event, KeyEvent.ACTION_UP));
                     mVolumeMusicControlActive = true;
+                case HardkeyActionHandler.MSG_FIRE_HOME:
+                    launchHomeFromHotKey(DEFAULT_DISPLAY);
+                    break;
+//                case HardkeyActionHandler.MSG_UPDATE_MENU_KEY:
+//                    synchronized (mLock) {
+//                        mHasPermanentMenuKey = msg.arg1 == 1;
+//                    }
+//                    break;
+                case HardkeyActionHandler.MSG_DO_HAPTIC_FB:
+                    performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, false, "Hardkey Long-Press");
                     break;
             }
         }
@@ -1903,6 +1918,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
         });
         mWakeGestureListener = new MyWakeGestureListener(mContext, mHandler);
+        // only for hwkey devices
+        if (!mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_showNavigationBar)) {
+            mKeyHandler = new HardkeyActionHandler(mContext, mHandler);
+        }
         mSettingsObserver = new SettingsObserver(mHandler);
         mSettingsObserver.observe();
         mShortcutManager = new ShortcutManager(context);
@@ -2796,10 +2816,27 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         final boolean down = event.getAction() == KeyEvent.ACTION_DOWN;
         final boolean canceled = event.isCanceled();
         final int displayId = event.getDisplayId();
+        final boolean longPress = (flags & KeyEvent.FLAG_LONG_PRESS) != 0;
+        final boolean virtualKey = event.getDeviceId() == KeyCharacterMap.VIRTUAL_KEYBOARD;
 
         if (DEBUG_INPUT) {
             Log.d(TAG, "interceptKeyTi keyCode=" + keyCode + " down=" + down + " repeatCount="
                     + repeatCount + " keyguardOn=" + keyguardOn + " canceled=" + canceled);
+        }
+
+        // we only handle events from hardware key devices that originate from
+        // real button
+        // pushes. We ignore virtual key events as well since it didn't come
+        // from a hard key or
+        // it's the key handler synthesizing a back or menu key event for
+        // dispatch
+        // if keyguard is showing and secure, don't intercept and let aosp keycode
+        // implementation handle event
+        if (mKeyHandler != null && !keyguardOn && !virtualKey) {
+            boolean handled = mKeyHandler.handleKeyEvent(win, keyCode, repeatCount, down, canceled,
+                    longPress, keyguardOn);
+            if (handled)
+                return -1;
         }
 
         // If we think we might have a volume down & power key chord on the way

Corvus-R/android_frameworks_base_test

Signed-off-by: mhkjahromi <m.h.k.jahromi@gmail.com>
Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -210,6 +210,9 @@ import com.android.internal.statusbar.IStatusBarService;
 import com.android.internal.util.ArrayUtils;
 import com.android.server.ExtconStateObserver;
 import com.android.server.ExtconUEventObserver;
+import com.android.internal.util.hwkeys.ActionHandler;
+import com.android.internal.util.hwkeys.ActionUtils;
+import com.android.internal.util.ScreenshotHelper;
 import com.android.server.GestureLauncherService;
 import com.android.server.LocalServices;
 import com.android.server.SystemServiceManager;
@@ -509,6 +512,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     boolean mWakeOnBackKeyPress;
     long mWakeUpToLastStateTimeout;
 
+    private HardkeyActionHandler mKeyHandler;
+
     private boolean mHandleVolumeKeysInWM;
 
     // Volume rocker wake
@@ -718,6 +723,16 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     dispatchMediaKeyWithWakeLockToAudioService(
                             KeyEvent.changeAction(event, KeyEvent.ACTION_UP));
                     mVolumeMusicControlActive = true;
+                case HardkeyActionHandler.MSG_FIRE_HOME:
+                    launchHomeFromHotKey(DEFAULT_DISPLAY);
+                    break;
+//                case HardkeyActionHandler.MSG_UPDATE_MENU_KEY:
+//                    synchronized (mLock) {
+//                        mHasPermanentMenuKey = msg.arg1 == 1;
+//                    }
+//                    break;
+                case HardkeyActionHandler.MSG_DO_HAPTIC_FB:
+                    performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, false, "Hardkey Long-Press");
                     break;
             }
         }
@@ -1731,6 +1746,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
         });
         mWakeGestureListener = new MyWakeGestureListener(mContext, mHandler);
+        // only for hwkey devices
+        if (!mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_showNavigationBar)) {
+            mKeyHandler = new HardkeyActionHandler(mContext, mHandler);
+        }
         mSettingsObserver = new SettingsObserver(mHandler);
         mSettingsObserver.observe();
         mModifierShortcutManager = new ModifierShortcutManager(context);
@@ -2700,10 +2720,27 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         final boolean canceled = event.isCanceled();
         final int displayId = event.getDisplayId();
         final long key_consumed = -1;
+        final boolean longPress = (flags & KeyEvent.FLAG_LONG_PRESS) != 0;
+        final boolean virtualKey = event.getDeviceId() == KeyCharacterMap.VIRTUAL_KEYBOARD;
 
         if (DEBUG_INPUT) {
             Log.d(TAG, "interceptKeyTi keyCode=" + keyCode + " down=" + down + " repeatCount="
                     + repeatCount + " keyguardOn=" + keyguardOn + " canceled=" + canceled);
+        }
+
+        // we only handle events from hardware key devices that originate from
+        // real button
+        // pushes. We ignore virtual key events as well since it didn't come
+        // from a hard key or
+        // it's the key handler synthesizing a back or menu key event for
+        // dispatch
+        // if keyguard is showing and secure, don't intercept and let aosp keycode
+        // implementation handle event
+        if (mKeyHandler != null && !keyguardOn && !virtualKey) {
+            boolean handled = mKeyHandler.handleKeyEvent(mKeyguardCandidate, keyCode, repeatCount, down, canceled,
+                    longPress, keyguardOn);
+            if (handled)
+                return -1;
         }
 
         if (mKeyCombinationManager.isKeyConsumed(event)) {