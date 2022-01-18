AOSPA/android_frameworks_base-old

input: introduce new navigation bar key event source
* When generating virtual navigation bar KeyEvent, set
  a specific source fort it. There is no precise way to
  recognize if the KeyEvent is coming from KeyButtonView.
  Another way is passing another flag.

Change-Id: I243c9f91afcbf4c430e2d526e8c814a5a7975e51
Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>

core/java/android/view/InputDevice.java
@@ -281,6 +281,15 @@ public final class InputDevice implements Parcelable {
     public static final int SOURCE_HDMI = 0x02000000 | SOURCE_CLASS_BUTTON;
 
     /**
+     * The input source is a specific virtual event sent from navigation bar.
+     *
+     * @see com.android.systemui.statusbar.policy.KeyButtonView#sendEvent()
+     * @author Carlo Savignano
+     * @hide
+     */
+    public static final int SOURCE_NAVIGATION_BAR = 0x04000000 | SOURCE_CLASS_BUTTON;
+
+    /**
      * A special input source constant that is used when filtering input devices
      * to match devices that provide any type of input source.
      */

Corvus-R/android_frameworks_base_test

Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

core/java/android/view/InputDevice.java
@@ -338,6 +338,15 @@ public final class InputDevice implements Parcelable {
     public static final int SOURCE_SENSOR = 0x04000000 | SOURCE_CLASS_NONE;
 
     /**
+     * The input source is a specific virtual event sent from navigation bar.
+     *
+     * @see com.android.systemui.statusbar.policy.KeyButtonView#sendEvent()
+     * @author Carlo Savignano
+     * @hide
+     */
+    public static final int SOURCE_NAVIGATION_BAR = 0x08000000 | SOURCE_CLASS_BUTTON;
+
+    /**
      * A special input source constant that is used when filtering input devices
      * to match devices that provide any type of input source.
      */